dfd2016928d823cf654f6f73c6f9b6078972d4f8
Lewis Staples <lewis.staples@gmail.com>
2022-11-24 22:38:52 -0500

    Day 01 Part A working

---
+++ day01/day01.py
@@ -0,0 +1,17 @@
+# adventOfCode 2018 day 1
+# https://adventofcode.com/2018/day/1
+
+freq_value = 0
+
+# Reading input from the input file
+input_filename='input.txt'
+print(f'\nUsing input file: {input_filename}\n')
+with open(input_filename) as f:
+    # Pull in each line from the input file
+    for in_string in f:
+        freq_change = int(in_string.rstrip().replace('+',''))
+        freq_value += freq_change
+
+print(f'The final frequency (answer to Part A) is: {freq_value}\n')
+
+

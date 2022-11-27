e5298b02a831d56389ce41fb133560d52cb005b7
Lewis Staples <lewis.staples@gmail.com>
2022-11-26 22:47:13 -0500

    Day 03: Getting all points for a given rectangular claim (written and tested). Getting counts of how often each point is claimed (written but not yet tested).

--- day03/day03.py
+++ day03/day03.py
@@ -28,10 +28,42 @@ def get_claims_from_input(input_filename):
             claims_collection[claim_id] = Claim(left_margin, top_margin, width, height)
     return claims_collection
 
-def solve_problem(input_filename):
-    claims = get_claims_from_input(input_filename)
+def get_claim_points(claim):
+    ret_val = set()
+    for x in range(claim.left_margin, claim.left_margin + claim.width):
+        for y in range(claim.top_margin, claim.top_margin + claim.height):
+            ret_val.add((x,y))
+    return ret_val
 
-solve_problem('input.txt')
+
+    dummy = 123
+
+def get_cpc_from_claims(claims):
+    claim_point_count = dict()
+    for claim_id, claim in claims.items():
+        for claim_point in get_claim_points(claim):
+            if claim_point in claim_point_count:
+                claim_point_count[claim_point] += 1
+            else:
+                claim_point_count[claim_point] = 1
+
+    return claim_point_count
+
+def solve_problem(input_filename):
+    # claim_point_count = dict()
+    claims = get_claims_from_input(input_filename)
+    claim_point_count = get_cpc_from_claims(claims)
+
+    dummy = 123
+
+    # for claim_id, claim in claims.items():
+    #     if claim in claim_point_count:
+    #         claim_point_count[claim] += 1
+    #     else:
+    #         claim_point_count[claim] = 1
+
+# solve_problem('input.txt')
+solve_problem('input_sample0.txt')
 
 def test_single_line_input():
     claims = get_claims_from_input('input_sample0.txt')
@@ -44,4 +76,19 @@ def test_multiple_line_input():
     assert claims['1'] == Claim(1, 3, 4, 4)
     assert claims['2'] == Claim(3, 1, 4, 4)
     assert claims['3'] == Claim(5, 5, 2, 2)
+
+def test_single_line_get_points():
+    claims = get_claims_from_input('input_sample0.txt')
+    assert len(claims) == 1
+    assert '123' in claims
+    assert (2,2) not in get_claim_points(claims['123'])
+    assert (3,1) not in get_claim_points(claims['123'])
+    assert (3,2) in get_claim_points(claims['123'])
+    assert (7,2) in get_claim_points(claims['123'])
+    assert (7,1) not in get_claim_points(claims['123'])
+    assert (8,2) not in get_claim_points(claims['123'])
+    assert (2,5) not in get_claim_points(claims['123'])
+    assert (3,5) in get_claim_points(claims['123'])
+    assert (7,5) in get_claim_points(claims['123'])
+    assert (8,5) not in get_claim_points(claims['123'])
 

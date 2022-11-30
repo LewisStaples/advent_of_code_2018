f0c15984dc63e1d83563bb05aeb766bad1186cde
Lewis Staples <lewis.staples@gmail.com>
2022-11-29 19:25:37 -0500

    Day 04: Part A complete

--- day04/day04.py
+++ day04/day04.py
@@ -10,7 +10,6 @@ def get_input_lines_sorted(input_filename):
         # Pull in each line from the input file
         for in_string in f:
             in_string = in_string.rstrip()
-            # in_string.replace(' 00:', ' 24:')
             ret_val.append(in_string)
     return sorted(ret_val)
 
@@ -28,7 +27,6 @@ def get_guard_records(sorted_records):
             assert record[12:14] == timestamp_sleep_started[11:13] == '00' # Assert hours are 00 (given)
             sleep_end_minutes = int(record[15:17])
             sleep_start_minutes = int(timestamp_sleep_started[14:])
-
             if current_guard not in ret_val:
                 ret_val[current_guard] = dict()
             for sleep_timestamp in range(sleep_start_minutes, sleep_end_minutes):
@@ -38,13 +36,28 @@ def get_guard_records(sorted_records):
                     ret_val[current_guard][sleep_timestamp] += 1
         else:
             raise ValueError(f'Strange input record:  {record}')
+    return ret_val
 
+def get_guard_id_with_most_total_sleep(guard_records):
+    # guard_totals is a temporary dict with guard_id and that guard's total time asleep
+    guard_totals = dict()
+    for guard in guard_records:
+        guard_totals[guard] = sum(guard_records[guard].values())
+    max_total = max(guard_totals.values())
+    ret_val = [k for (k,v) in guard_totals.items() if v == max_total][0]
+    return ret_val
+
+def get_minute_most_sleep(guard_id, guard_records):
+    max_count = max(guard_records[guard_id].values())
+    ret_val = [k for (k,v) in guard_records[guard_id].items() if v == max_count][0]
     return ret_val
 
 def solve_problem(input_filename):
     sorted_records = get_input_lines_sorted(input_filename)
     guard_records = get_guard_records(sorted_records)
-    dummy = 123
+    guard_id_with_most_total_sleep = get_guard_id_with_most_total_sleep(guard_records)
+    minute_that_most_sleepy_guard_is_most_likely_asleep = get_minute_most_sleep(guard_id_with_most_total_sleep, guard_records)
+    print(f'The answer to part A is {guard_id_with_most_total_sleep * minute_that_most_sleepy_guard_is_most_likely_asleep}\n')
 
 solve_problem('input_sample0.txt')
 

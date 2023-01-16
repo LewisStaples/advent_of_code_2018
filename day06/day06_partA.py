d30147b5d0e7eb2a680f7dc6fe476b0a3e883eb0
Lewis Staples <lewis.staples@gmail.com>
2023-01-15 20:48:57 -0500

    Day 06 Code cleanup

--- day06/day06_partA.py
+++ day06/day06_partA.py
@@ -13,106 +13,103 @@ point_collection = dict()
 
 def get_input(input_filename):
     # Reading input from the input file
-    print(f'\nUsing input file: {input_filename}\n')
+    print(f"\nUsing input file: {input_filename}\n")
     with open(input_filename) as f:
         # Pull in each line from the input file
         for group_number, in_string in enumerate(f):
             in_string = in_string.rstrip()
-            x,y = in_string.split(', ')
-            x,y = (int(z) for z in (x,y))
-            point_collection[(x,y)] = {'group_number': group_number, 'manh_dist': 0}
+            x, y = in_string.split(", ")
+            x, y = (int(z) for z in (x, y))
+            point_collection[(x, y)] = {"group_number": group_number, "manh_dist": 0}
     return point_collection
+
 
 def get_outer_box(point_collection):
     ret_val = {
-        'x_min': float('inf'),
-        'x_max': float('-inf'),
-        'y_min': float('inf'),
-        'y_max': float('-inf'),
+        "x_min": float("inf"),
+        "x_max": float("-inf"),
+        "y_min": float("inf"),
+        "y_max": float("-inf"),
     }
-    for x,y in point_collection:
-        ret_val['x_min'] = min(ret_val['x_min'], x)
-        ret_val['x_max'] = max(ret_val['x_max'], x)
-        ret_val['y_min'] = min(ret_val['y_min'], y)
-        ret_val['y_max'] = max(ret_val['y_max'], y)
+    for x, y in point_collection:
+        ret_val["x_min"] = min(ret_val["x_min"], x)
+        ret_val["x_max"] = max(ret_val["x_max"], x)
+        ret_val["y_min"] = min(ret_val["y_min"], y)
+        ret_val["y_max"] = max(ret_val["y_max"], y)
     return ret_val
 
+
 def special_print(set_pts, point_collection, manh_dist):
-    '''
+    """
     Used for debugging, if needed
-    '''
-    the_groups = list({x['group_number'] for x in point_collection.values()})
-    while(None in the_groups):
+    """
+    the_groups = list({x["group_number"] for x in point_collection.values()})
+    while None in the_groups:
         the_groups.remove(None)
-    
+
     for group in the_groups:
-        print(f'Grp {group}: ', end = '')
+        print(f"Grp {group}: ", end="")
         for k, v in point_collection.items():
-            if v['group_number'] == group:
-                if v['manh_dist'] == manh_dist:
-                    print(k, end = ', ')
+            if v["group_number"] == group:
+                if v["manh_dist"] == manh_dist:
+                    print(k, end=", ")
         print()
     print(set_pts)
+
 
 def extend_til_shown_infinite(point_collection, outer_box):
     manh_dist = 0
     while True:
+        set_pts = {
+            k
+            for k, v in point_collection.items()
+            if v["manh_dist"] == manh_dist and v["group_number"] is not None
+        }
 
-        set_pts = {k for k,v in point_collection.items() if v['manh_dist'] == manh_dist and v['group_number'] is not None}
         # if all set_pts outside outer_box, return set_pts
-        
-        
         flag = True
         for pt in set_pts:
-            if pt[0] <= outer_box['x_max']:
-                if pt[0] >= outer_box['x_min']:
-                    if pt[1] <= outer_box['y_max']:
-                        if pt[1] >= outer_box['y_min']:
+            if pt[0] <= outer_box["x_max"]:
+                if pt[0] >= outer_box["x_min"]:
+                    if pt[1] <= outer_box["y_max"]:
+                        if pt[1] >= outer_box["y_min"]:
                             flag = False
-        
         if flag:
-            # special_print(set_pts, point_collection, manh_dist)
-            # print()
             return set_pts
 
-        
         manh_dist += 1
-        # print(f'Manhattan Distance: {manh_dist}')
-
         for pt in set_pts:
             # find points one step away
-            for direction in [[0,-1],[0,1],[1,0],[-1,0]]:
-                new_pt = (
-                    pt[0] + direction[0],
-                    pt[1] + direction[1]
-                )
+            for direction in [[0, -1], [0, 1], [1, 0], [-1, 0]]:
+                new_pt = (pt[0] + direction[0], pt[1] + direction[1])
                 if new_pt in point_collection:
-                    if point_collection[new_pt]['group_number'] == point_collection[pt]['group_number']:
+                    if (
+                        point_collection[new_pt]["group_number"]
+                        == point_collection[pt]["group_number"]
+                    ):
                         continue
-                    if point_collection[new_pt]['manh_dist'] == manh_dist:
-                        point_collection[new_pt]['group_number'] = None
+                    if point_collection[new_pt]["manh_dist"] == manh_dist:
+                        point_collection[new_pt]["group_number"] = None
                     else:
-                        assert(point_collection[new_pt]['manh_dist'] < manh_dist)
+                        assert point_collection[new_pt]["manh_dist"] < manh_dist
                 else:
-                    point_collection[new_pt] = {'group_number': point_collection[pt]['group_number'], 'manh_dist': manh_dist}
-        
-        # special_print(set_pts, point_collection, manh_dist)
-        # print()
-        # print(f'Manhattan Distance: {manh_dist}')
-        
-        # break
-    # print({k for k,v in point_collection.items() if v['manh_dist'] == manh_dist and v['group_number'] is not None})
+                    point_collection[new_pt] = {
+                        "group_number": point_collection[pt]["group_number"],
+                        "manh_dist": manh_dist,
+                    }
+
 
 def get_infinite_groups(point_collection, last_pts):
     infinite_groups = set()
     for l_pt in last_pts:
-        infinite_groups.add(point_collection[l_pt]['group_number'])
+        infinite_groups.add(point_collection[l_pt]["group_number"])
     return infinite_groups
+
 
 def get_size_largest_noninfinite_group(point_collection, infinite_groups):
     group_sizes = dict()
     for point in point_collection:
-        group_number = point_collection[point]['group_number']
+        group_number = point_collection[point]["group_number"]
         if group_number in infinite_groups:
             continue
         if group_number is None:
@@ -124,16 +121,20 @@ def get_size_largest_noninfinite_group(point_collection, infinite_groups):
 
     return max(group_sizes.values())
 
+
 def solve_problem(input_filename):
     point_collection = get_input(input_filename)
     outer_box = get_outer_box(point_collection)
     last_pts = extend_til_shown_infinite(point_collection, outer_box)
     infinite_groups = get_infinite_groups(point_collection, last_pts)
-    size_largest_noninfinite_group = get_size_largest_noninfinite_group(point_collection, infinite_groups)
+    size_largest_noninfinite_group = get_size_largest_noninfinite_group(
+        point_collection, infinite_groups
+    )
 
-    print(f'The size of the largest non-infinite group is: {size_largest_noninfinite_group}\n')
+    print(
+        f"The size of the largest non-infinite group is: \
+            {size_largest_noninfinite_group}\n"
+    )
 
-solve_problem('input_sample0.txt')
 
-
-    
+solve_problem("input_sample0.txt")
--- day06/day06_partB.py
+++ day06/day06_partB.py
@@ -3,102 +3,121 @@
 # adventOfCode 20xy day 6
 # https://adventofcode.com/20xy/day/6
 
+
 def get_given_coordinates(input_filename):
-    given_coordinates = set() #  dict() # set()
+    """
+    This reads the given coordinates from the text file,
+    and returns a set of tuples with these points
+    """
+    given_coordinates = set()
     # Reading input from the input file
-    print(f'\nUsing input file: {input_filename}\n')
+    print(f"\nUsing input file: {input_filename}\n")
     with open(input_filename) as f:
         # Pull in each line from the input file
         for in_string in f:
             in_string = in_string.rstrip()
-            x,y = in_string.split(', ')
-            x,y = (int(z) for z in (x,y))
-            given_coordinates.add((x,y))
-            # given_coordinates[(x,y)] = True
-
+            x, y = in_string.split(", ")
+            x, y = (int(z) for z in (x, y))
+            given_coordinates.add((x, y))
     return given_coordinates
 
+
 def get_outer_box(given_coordinates):
+    """
+    Returns the outer bounding box of all the set of coordinates
+    """
     ret_val = {
-        'x_min': float('inf'),
-        'x_max': float('-inf'),
-        'y_min': float('inf'),
-        'y_max': float('-inf'),
+        "x_min": float("inf"),
+        "x_max": float("-inf"),
+        "y_min": float("inf"),
+        "y_max": float("-inf"),
     }
-    for x,y in given_coordinates:
-        ret_val['x_min'] = min(ret_val['x_min'], x)
-        ret_val['x_max'] = max(ret_val['x_max'], x)
-        ret_val['y_min'] = min(ret_val['y_min'], y)
-        ret_val['y_max'] = max(ret_val['y_max'], y)
+    for x, y in given_coordinates:
+        ret_val["x_min"] = min(ret_val["x_min"], x)
+        ret_val["x_max"] = max(ret_val["x_max"], x)
+        ret_val["y_min"] = min(ret_val["y_min"], y)
+        ret_val["y_max"] = max(ret_val["y_max"], y)
     return ret_val
 
-def manh_dist(init_point, point):
-    return abs(init_point[0] - point[0]) + abs(init_point[1] - point[1])
+
+def manh_dist(point1, point2):
+    """
+    Calculate the manhattan distance
+    """
+    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+
 
 def tot_manh_dist_in_range(given_coordinates, point):
+    """
+    Returns boolean, indicating whether the sum of all manhattan
+    distances from each given coordinate to the point is within
+    the required threshold.
+
+    I substracted one from the "less than" value in the spec, so
+    I could name the variable max_manh_dist
+    """
     total_manh_dist = 0
     if len(given_coordinates) == 6:
-        max_manh_dist = 31 # ungraded sample input
+        max_manh_dist = 31  # ungraded sample input ("less than 32" from description)
     elif len(given_coordinates) == 50:
-        max_manh_dist = 9999 # graded input
+        max_manh_dist = 9999  # graded input ("less than 10000" from description)
     for init_point in given_coordinates:
         total_manh_dist += manh_dist(init_point, point)
     return total_manh_dist <= max_manh_dist
 
 
 def get_points_on_box_perimeter(given_coordinates, outer_box):
-    ret_val = set() # dict()
-    for x in range(outer_box['x_min'], outer_box['x_max'] + 1):
-        y = outer_box['y_min']
-        if tot_manh_dist_in_range(given_coordinates, (x,y)):
-            # ret_val[(x,y)] = True
-            ret_val.add((x,y))
-        # else:
-        #     ret_val[(x,y)] = False
-        y = outer_box['y_max']
-        if tot_manh_dist_in_range(given_coordinates, (x,y)):
-            # ret_val[(x,y)] = True
-            ret_val.add((x,y))
-        # else:
-        #     ret_val[(x,y)] = False
-    for y in range(outer_box['y_min'], outer_box['y_max'] + 1):
-        x = outer_box['x_min']
-        if tot_manh_dist_in_range(given_coordinates, (x,y)):
-            # ret_val[(x,y)] = True
-            ret_val.add((x,y))
-        # else:
-        #     ret_val[(x,y)] = False
-        x = outer_box['x_max']
-        if tot_manh_dist_in_range(given_coordinates, (x,y)):
-            # ret_val[(x,y)] = True
-            ret_val.add((x,y))
-        # else:
-        #     ret_val[(x,y)] = False
-
-    return ret_val
-def get_points_inside_box_perimeter(given_coordinates, outer_box):
-    # ret_val = dict()
+    """
+    This checks all points in outer_box, and it returns a set listing all
+    such points for whom tot_manh_dist_in_range threshold is met.
+    """
     ret_val = set()
-    for x in range(outer_box['x_min'] + 1, outer_box['x_max']):
-        for y in range(outer_box['y_min'] + 1, outer_box['y_max']):
-            if tot_manh_dist_in_range(given_coordinates, (x,y)):
-                # ret_val[(x,y)] = True
-                ret_val.add((x,y))
-            # else:
-            #     ret_val[(x,y)] = False
+    for x in range(outer_box["x_min"], outer_box["x_max"] + 1):
+        y = outer_box["y_min"]
+        if tot_manh_dist_in_range(given_coordinates, (x, y)):
+            ret_val.add((x, y))
+        y = outer_box["y_max"]
+        if tot_manh_dist_in_range(given_coordinates, (x, y)):
+            ret_val.add((x, y))
+    for y in range(outer_box["y_min"], outer_box["y_max"] + 1):
+        x = outer_box["x_min"]
+        if tot_manh_dist_in_range(given_coordinates, (x, y)):
+            ret_val.add((x, y))
+        x = outer_box["x_max"]
+        if tot_manh_dist_in_range(given_coordinates, (x, y)):
+            ret_val.add((x, y))
     return ret_val
+
+
+def get_points_inside_box_perimeter(given_coordinates, outer_box):
+    """
+    This checks all points inside the outer_box (excluding the
+    perimeter), and it returns a set listing all
+    such points for whom tot_manh_dist_in_range threshold is met.
+    """
+    ret_val = set()
+    for x in range(outer_box["x_min"] + 1, outer_box["x_max"]):
+        for y in range(outer_box["y_min"] + 1, outer_box["y_max"]):
+            if tot_manh_dist_in_range(given_coordinates, (x, y)):
+                ret_val.add((x, y))
+    return ret_val
+
 
 def solve_problem(input_filename):
     given_coordinates = get_given_coordinates(input_filename)
     outer_box = get_outer_box(given_coordinates)
-    points_total_manh_dist_lt_threshold = get_points_on_box_perimeter(given_coordinates, outer_box)
-    # if points_total_manh_dist_lt_threshold is None:
-    # if True not in points_total_manh_dist_lt_threshold.values():
+    points_total_manh_dist_lt_threshold = get_points_on_box_perimeter(
+        given_coordinates, outer_box
+    )
     if len(points_total_manh_dist_lt_threshold) == 0:
-        # Don't need to consider points outside of the box
-        points_total_manh_dist_lt_threshold = get_points_inside_box_perimeter(given_coordinates, outer_box)
+        # If true, don't need to consider points outside of the box.  If there were any
+        # points on the perimeter that pass the threshold it should suffice to only
+        # such their immediate neighbors if they pass the threshold, and then check
+        # their neighbors, but that was not required for the given input
+        points_total_manh_dist_lt_threshold = get_points_inside_box_perimeter(
+            given_coordinates, outer_box
+        )
+        print(f"The answer to part B is {len(points_total_manh_dist_lt_threshold)}\n")
 
-        print(f'The answer to part B is {len(points_total_manh_dist_lt_threshold)}\n')
 
-solve_problem('input.txt')
-
+solve_problem("input_sample0.txt")

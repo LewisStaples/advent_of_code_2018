72525e8786d62b2555d85f7bb7443495faba5c01
Lewis Staples <lewis.staples@gmail.com>
2023-01-24 20:47:02 -0500

    Day 09 Parts A and B complete.  Replaced earlier version that used Python built in list with a custom implementation of a linked list of MarbleNode objects

--- day09/day09.py
+++ day09/day09.py
@@ -4,75 +4,114 @@
 # https://adventofcode.com/20xy/day/9
 
 
-from dataclasses import dataclass
-
-
 def get_input_values(input_filename):
-    print(f'\nUsing input file: {input_filename}\n')
+    print(f"\nUsing input file: {input_filename}\n")
     ret_val = list()
     with open(input_filename) as f:
         while True:
             in_string = f.readline().rstrip()
             if len(in_string) == 0:
                 break
-            player_count, str2 = in_string.split(' players; last marble is worth ')
-            last_marble_value, str3 = str2.split(' points')
+            player_count, str2 = in_string.split(" players; last marble is worth ")
+            last_marble_value, str3 = str2.split(" points")
             ret_val.append(tuple((int(player_count), int(last_marble_value))))
     return ret_val
 
-@dataclass
-class Circle:
-    current_index: int
-    marble_circle: list
 
-    def update(self, next_marble):
-        '''
+class MarbleNode:
+    def __init__(self, number):
+        self.number = number
+        self.next = None
+        self.prev = None
+
+    def label_next(self, next):
+        self.next = next
+
+    def label_prev(self, prev):
+        self.prev = prev
+
+    def __repr__(self):
+        return str(self.number)
+
+
+class Circle:
+    def __init__(self):
+        self.marble_node = MarbleNode(0)
+        self.current_node = self.marble_node
+        self.ORIGINAL_NODE = self.marble_node  # for printing
+
+        self.marble_node.next = self.marble_node
+        self.marble_node.prev = self.marble_node
+
+    def update(self, latest_marble_value):
+        """
         The update method handles updates to the circle of marbles in a particular turn.
         The return value is the amount to be added to the player's score.
-        '''
+        """
 
         # Update circle by removing a marble (don't insert next_marble)
-        if next_marble % 23 == 0:
-            ret_val = next_marble
-            self.current_index -= 7
-            self.current_index %= len(self.marble_circle)
-            # print(f'Removing marble {self.marble_circle[self.current_index]} from index {self.current_index} ...')
-            ret_val += self.marble_circle.pop(self.current_index)
+        if latest_marble_value % 23 == 0:
+            ret_val = latest_marble_value
+
+            # Adjust current node CCW by 7 marbles
+            for _ in range(7):
+                self.current_node = self.current_node.prev
+
+            # Include current node in player's score and remove it from the circle
+            ret_val += self.current_node.number
+            prior_node = self.current_node.prev
+            next_node = self.current_node.next
+            prior_node.label_next(next_node)
+            next_node.label_prev(prior_node)
+            self.current_node = next_node
             return ret_val
-        
-        # Update by inserting a new marble
-        self.current_index = (self.current_index + 2) % len(self.marble_circle)
-        self.marble_circle.insert(self.current_index, next_marble)
+
+        latest_marble_node = MarbleNode(latest_marble_value)
+        self.current_node = self.current_node.next
+        current_next = self.current_node.next
+
+        latest_marble_node.label_next(current_next)
+        latest_marble_node.label_prev(self.current_node)
+        self.current_node.label_next(latest_marble_node)
+        current_next.label_prev(latest_marble_node)
+
+        self.current_node = latest_marble_node
         return 0
 
     def __repr__(self):
-        ret_val = '['
-        i_zero = self.marble_circle.index(0)
-        for i in (list(range(i_zero, len(self.marble_circle))) + list(range(i_zero))):
-            val = self.marble_circle[i]
-            this_portion = f'{val}'
-            if i == self.current_index:
-                this_portion = '(' + this_portion + ')'
-
-            this_portion += ', '
-
-            ret_val += this_portion
-
-        ret_val += ']'
+        ret_val = "["
+        this_node = self.ORIGINAL_NODE
+        ret_val += str(this_node) + ", "
+        while this_node.next != self.ORIGINAL_NODE:
+            this_node = this_node.next
+            if this_node == self.current_node:
+                ret_val += "(" + str(this_node) + "), "
+            else:
+                ret_val += str(this_node) + ", "
+        ret_val += "]"
         return ret_val
+
 
 def solve_problem(i_v):
     player_count, last_marble_value = i_v
-    print(f'Using player count {player_count}, and last marble value {last_marble_value}: ', end='')
-    circle = Circle(0, [0])
-    player_scores = {x:0 for x in range(1, player_count + 1)}
+    print(
+        f"With player count {player_count}, and last marble value {last_marble_value}: "
+    )
+    circle = Circle()
+    player_scores = {x: 0 for x in range(1, player_count + 1)}
     for next_marble in range(1, last_marble_value + 1):
         player_number = (next_marble - 1) % player_count + 1
         player_scores[player_number] += circle.update(next_marble)
-        # print(f'Player {player_number}, circle: ', end='')
-        # print(circle)
-    # print()
-    print(f'high score is {max(player_scores.values())}')
+
+    print(f"Part 1 / Part A high score is {max(player_scores.values())}")
+
+    circle = Circle()
+    player_scores = {x: 0 for x in range(1, player_count + 1)}
+    for next_marble in range(1, 100 * last_marble_value + 1):
+        player_number = (next_marble - 1) % player_count + 1
+        player_scores[player_number] += circle.update(next_marble)
+    print(f"Part 1 / Part B high score is {max(player_scores.values())}")
+
 
 def open_input_file(input_filename):
     input_values = get_input_values(input_filename)
@@ -81,6 +120,4 @@ def open_input_file(input_filename):
     print()
 
 
-open_input_file('input.txt')
-
-
+open_input_file("input_sample0.txt")

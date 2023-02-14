9d001f0a3c813f119371e7ef0011359bbde7fcf9
Lewis Staples <lewis.staples@gmail.com>
2023-02-14 14:28:31 -0500

    Day 11 Part 2/B  Summed-area table function working for a sample that I found online

--- day11/day11.py
+++ day11/day11.py
@@ -36,6 +36,31 @@ def get_sum(fuel_cell_grid, dimension_in, x_in, y_in):
                 dummy = 123
     return sum
 
+def get_summed_area_table(input_table):
+    # Require that input_table is a list of lists of int 
+    assert type(input_table) == list
+    assert type(input_table[0]) == list
+    assert type(input_table[0][0]) == int
+
+    summed_area__table = list()
+    for i in range(len(input_table)):
+        summed_area__table.append(list())
+        for j in range(len(input_table[0])):
+            new_value = input_table[i][j]
+            if i > 0:
+                new_value += summed_area__table[i-1][j]
+            if j > 0:
+                new_value += summed_area__table[i][j-1]
+            if (i > 0) and (j > 0):
+                new_value -= summed_area__table[i-1][j-1]
+            # Modify below to include values to the left and above of it
+            # new_value = input_table[i][j]
+            # if i > 0:
+            #     new_value += summed_area__table[i-1][j]
+            summed_area__table[i].append(new_value)
+            # print(summed_area__table)
+    return summed_area__table
+
 def get_answers(fuel_cell_grid):
     best_seen = {
         'highest_sum': float('-inf'),
@@ -58,21 +83,51 @@ def get_answers(fuel_cell_grid):
 
     # Part 2 / B:
 
-    # Create a summed-area table
-    summed_area__fuel_cell_grid = list()
-    for i in range(300):
-        summed_area__fuel_cell_grid.append(list())
-        for j in range(300):
-            # Modify below to include values to the left and above of it
-            new_value = fuel_cell_grid[i][j]
-            summed_area__fuel_cell_grid[i].append(new_value)
+    summed_area__fuel_cell_grid = get_summed_area_table(fuel_cell_grid)
+    # # Create a summed-area table
+    # summed_area__fuel_cell_grid = list()
+    # for i in range(300):
+    #     summed_area__fuel_cell_grid.append(list())
+    #     for j in range(300):
+    #         # Modify below to include values to the left and above of it
+    #         new_value = fuel_cell_grid[i][j]
+    #         summed_area__fuel_cell_grid[i].append(new_value)
     
     return part_one , 'not-yet-solved'
-    
-serial_number = get_serial_number('input.txt')
-fuel_cell_grid = get_grid(serial_number)
 
-answer_part1 , answer_part2 = get_answers(fuel_cell_grid)
-print(f'The answer to part 1 (A) is: {answer_part1}')
-print(f'The answer to part 2 (B) is: {answer_part2}')
+def display(table):
+    for i in range(len(table)):
+        for j in range(len(table[0])):
+            print(f'{table[i][j]} ', end = ' ')
+        print()
+    print()
+
+def test__get_summed_area_table():
+    # Using example from slide 3 on the below PDF
+    # https://developer.amd.com/wordpress/media/2012/10/GDC2005_SATEnvironmentReflections.pdf
+    init_table = list()
+    init_table.append([2, 3, 2, 1])
+    init_table.append([3, 0, 1, 2])
+    init_table.append([1, 3, 1, 0])
+    init_table.append([1, 4, 2, 2])
+
+    # print()
+    # display(init_table)
+
+    s_a_t = get_summed_area_table(init_table)
+    # display(s_a_t)
+    assert s_a_t[0] == [2,5,7,8]
+    assert s_a_t[1] == [5,8,11,14]
+    assert s_a_t[2] == [6,12,16,19]
+    assert s_a_t[3] == [7,17,23,28]
+
+    # print( s_a_t[0] )
+
+
+# serial_number = get_serial_number('input.txt')
+# fuel_cell_grid = get_grid(serial_number)
+
+# answer_part1 , answer_part2 = get_answers(fuel_cell_grid)
+# print(f'The answer to part 1 (A) is: {answer_part1}')
+# print(f'The answer to part 2 (B) is: {answer_part2}')
 

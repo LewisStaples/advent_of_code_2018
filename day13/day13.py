60b4d430a3c75124a5e9ec482986724bb3ac5e1b
Lewis Staples <lewis.staples@gmail.com>
2023-02-19 23:36:56 -0500

    Day 13 Started to implement cart motion

--- day13/day13.py
+++ day13/day13.py
@@ -5,6 +5,7 @@
 
 
 import numpy as np
+import copy
 
 
 def get_input_line(input_filename):
@@ -19,11 +20,11 @@ def get_input_line(input_filename):
             print(in_string)
             for col_number, ch in enumerate(in_string):
                 if ch in ['v','^']:
-                    carts.append([ch,[col_number, row_number]])
-                    tracks[(col_number, row_number)]  = ch
+                    carts.append([ch,np.array([col_number, row_number])])
+                    tracks[(col_number, row_number)]  = '|'
                 elif ch in ['<','>']:
-                    carts.append([ch,[col_number, row_number]])
-                    tracks[(col_number, row_number)] = ch
+                    carts.append([ch,np.array([col_number, row_number])])
+                    tracks[(col_number, row_number)] = '-'
                 elif ch == ' ':
                     pass
                 else:
@@ -31,9 +32,52 @@ def get_input_line(input_filename):
     print()
 
     return tracks, [carts]
-    
+
+
+def get_first_crash_location(tracks, old_cart_collection_list):
+    while True:
+        # Take a new step forward in time
+        # Compose a new list of all possible states of where carts could be at the end of this clock tick
+        new_cart_collection_list = list()
+        # Loop through each of all possible states of where carts could be at the start of this clock tick
+        for old_cart_collection in old_cart_collection_list:
+            new_cart_collection = old_cart_collection
+            # For starting position of one cart in one possible state of all carts
+            for old_cart in old_cart_collection:
+                old_track_ch, old_location = old_cart
+                old_location = old_cart[1]
+                old_track_ch = tracks[tuple(old_location)]
+                if old_track_ch == '+':
+                    # NEEDED FOR NEXT STEP .... ADD SYMBOL (<  >  v  ^)
+                    for i, step in enumerate((
+                        np.array([0,1]),
+                        np.array([0,-1]),
+                        np.array([1,0]),
+                        np.array([-1,0]),
+                    )):
+                        # copy.deepcopy(old_cart_collection).append(['<',old_location + step])
+                        if i < 3:
+                            new_cart_collection = copy.deepcopy(old_cart_collection)
+                        new_cart_collection.append(['<',old_location + step])
+                        dummy = 123
+                        new_cart_collection_list.append(new_cart_collection)
+
+                # Implement later .....
+                elif old_track_ch in ['\\','/']:
+                    pass
+                else:
+                    assert(old_track_ch in ['|','-'])
+                    pass
+
+
+
+        break
+    return ''
+
+
 def solve_problem(input_filename):
     tracks, cart_collection_list = get_input_line(input_filename)
+    print(f'Answer to A/1: {get_first_crash_location(tracks, cart_collection_list)}')
     dummy = 123
 
 # solve_problem('input_sample0.txt')

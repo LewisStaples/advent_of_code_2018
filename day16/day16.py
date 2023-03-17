07984454af3406c7265c3ec668be33f816e65006
Lewis Staples <lewis.staples@gmail.com>
2023-03-16 23:49:25 -0400

    Day 16  Distinguishing between getting value vs. getting register (really looking up the value, then going to the register at that address, and getting the value stored there)

--- day16/day16.py
+++ day16/day16.py
@@ -19,9 +19,17 @@ def get_number_list(in_string):
     return the_number_list
     
 
-def get_final_register_values(register_values, instruction_values):
-    # To be implemented later ....
-    return -42
+def get_value(register_values, register_number):
+    return register_values[register_number]
+
+def get_register(register_values, register_number):
+    the_value = get_value(register_values, register_number)
+    return register_values[the_value]
+
+def get_opcode_succ_count(register_values, instruction_values, in_string):
+    opcode_succ_count = 0
+    return opcode_succ_count
+
 
 def get_count_opcodes_with_result(input_filename):
     # Reading input from the input file
@@ -34,6 +42,8 @@ def get_count_opcodes_with_result(input_filename):
         # Pull in each line from the input file
         for line_number, in_string in enumerate(f):
             in_string = in_string.rstrip()
+            if TEST:
+                print(in_string)
             match line_number % 4:
                 case 0:
                     if in_string[:7] != 'Before:':
@@ -42,10 +52,12 @@ def get_count_opcodes_with_result(input_filename):
                 case 1:
                     instruction_values = get_number_list(in_string)
                 case 2:
-                    if get_final_register_values(register_values, instruction_values) == get_number_list(in_string):
-                        opcode_succ_count += 1
-            if TEST:
-                print(in_string)
+                    opcode_succ_count = get_opcode_succ_count(register_values, instruction_values, in_string)
+                    if TEST:
+                        print(f'{opcode_succ_count = }')
+                    # if get_final_register_values(register_values, instruction_values) == get_number_list(in_string):
+                    #     opcode_succ_count += 1
+
 
         # Sample test program ... not used in part 1
 
@@ -56,7 +68,11 @@ def solve_problem(input_filename):
 
 
 
-solve_problem('input_scenario0.txt')
+solve_problem('input_sample0.txt')
 
-# def test_sample_0():
-#     solve_problem('input_sample0.txt')
+def test_get_value():
+    assert get_value([0,3,7,11], 1) == 3
+
+def test_get_register():
+    assert get_register([0,3,7,11], 1) == 11
+

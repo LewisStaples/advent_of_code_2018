5dd029ff59f0c124daf91921949ef197720cbc8f
Lewis Staples <lewis.staples@gmail.com>
2023-03-20 23:25:52 -0400

    Function binary is now modifying register_values with the result from the operator.  The bitwise binary operators have been added.

--- day16/day16.py
+++ day16/day16.py
@@ -5,8 +5,14 @@
 
 TEST = False
 TEST = True
+
+import copy
+
 # Storing the name of each operation, followed by the type of operator:  binary, unary, or conditional
-ALL_OPCODES = {'addr': 'binary', 'addi': 'binary', 'mulr': 'binary', 'muli':'binary'}
+
+ALL_OPCODES = {'addr': 'binary', 'addi': 'binary', 'mulr': 'binary', 'muli':'binary', 
+               'banr':'binary', 'bani':'binary', 'borr':'binary', 'bori':'binary',
+               }
 
 def get_number_list(in_string):
     in_string = in_string.replace(',', '')
@@ -16,7 +22,6 @@ def get_number_list(in_string):
     # Truncate all text to the right of the last digit
     while not in_string[-1].isdigit():
         in_string = in_string[:-1]
-    # print(in_string)opcode_succ_count
     the_number_list = [int(num) for num in in_string.split(' ')]
     return the_number_list
     
@@ -28,6 +33,9 @@ def get_register(instruction_values, instruction_number, register_values):
     register_number = get_value(instruction_values, instruction_number)
     return register_values[register_number]
 
+def set_register(instruction_values, instruction_number, register_values, value_to_set):
+    register_number = get_value(instruction_values, instruction_number)
+    register_values[register_number] = value_to_set
 
 def add(left_value, right_value):
     return left_value + right_value
@@ -35,24 +43,31 @@ def add(left_value, right_value):
 def mul(left_value, right_value):
     return left_value * right_value
 
-def binary(register_values_initial, register_values_expected_final, instruction_values, opcode_name):
+def ban(left_value, right_value):
+    return left_value & right_value
+
+def bor(left_value, right_value):
+    return left_value | right_value
+
+def binary(register_values, instruction_values, opcode_name):
     # Implement logic to strip off the last letter and call function with the remaining letters
     # and use that last letter to determine which parameters will be sent
-    left_value = get_register(instruction_values, 1, register_values_initial)
+    left_value = get_register(instruction_values, 1, register_values)
     if opcode_name[-1] == 'r':
-        right_value = get_register(instruction_values, 2, register_values_initial)
+        right_value = get_register(instruction_values, 2, register_values)
     else:
         right_value = get_value(instruction_values, 2)
     function_to_call = opcode_name[:-1]
-    register_index = instruction_values[3]
-    return eval(f'{function_to_call}(left_value,right_value)') == register_values_expected_final[register_index]
-
+    result = eval(f'{function_to_call}(left_value,right_value)')
+    set_register(instruction_values, 3, register_values, result)
+    return register_values
 
 def get_opcode_succ_list(register_values_initial, instruction_values, register_values_expected_final):
     opcode_succ_list = list()
     for opcode_name, operator_type in ALL_OPCODES.items():
-        print(f'{opcode_name = } , {operator_type = }')
-        if eval(f'{operator_type}( {register_values_initial}, {register_values_expected_final}, {instruction_values}, opcode_name )'):
+        register_values = copy.deepcopy(register_values_initial)
+        register_values = eval(f'{operator_type}( {register_values}, {instruction_values}, opcode_name )')
+        if register_values_expected_final == register_values:
             opcode_succ_list.append(opcode_name)
     return opcode_succ_list
 
@@ -78,12 +93,11 @@ def get_count_opcodes_with_result(input_filename):
                 case 1:
                     instruction_values = get_number_list(in_string)
                 case 2:
-                    opcode_succ_count += len(get_opcode_succ_list(register_values, instruction_values, get_number_list(in_string)))
+                    matching_opcodes = get_opcode_succ_list(register_values, instruction_values, get_number_list(in_string))
+                    opcode_succ_count += len(matching_opcodes)
                     if TEST:
-                        print(f'{opcode_succ_count = }')
-                    # if get_final_register_values(register_values, instruction_values) == get_number_list(in_string):
-                    #     opcode_succ_count += 1
-
+                        print(f'The count of opcodes that match is: {opcode_succ_count}  ;  ', end = '')
+                        print(f'Matching opcodes: {matching_opcodes}')
 
         # Sample test program ... not used in part 1
 

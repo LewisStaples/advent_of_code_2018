e4d532633a13832c85e9b137e914739532a1b8db
Lewis Staples <lewis.staples@gmail.com>
2023-04-15 23:29:44 -0400

    Day 20  Map creation:  getting rooms and doors traversed along its path

--- day20/day20.py
+++ day20/day20.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+
+# adventOfCode 2018 day 20
+# https://adventofcode.com/2018/day/20
+
+
+import numpy as np
+
 def get_input_line(input_filename):
     print(f'\nUsing input file: {input_filename}\n')
     with open(input_filename) as f:
@@ -6,9 +14,22 @@ def get_input_line(input_filename):
     return in_string[1:-1]
 
 
+def get_vector(ch):
+    match ch:
+        case 'N': return np.array([0,1])
+        case 'E': return np.array([1,0])
+        case 'S': return np.array([0,-1])
+        case 'W': return np.array([-1,0])
+
+def get_door(vector):
+    if abs(vector[0]) == 1:
+        return '|'
+    elif abs(vector[1]) == 1:
+        return '-'
+
 def get_map(in_string):
     ret_val = dict()
-    curr_locn = [0,0]
+    curr_locn = np.array([0,0])
     ret_val[tuple(curr_locn)] = 'X'
     for ch in in_string:
         if ch == '(':
@@ -16,17 +37,23 @@ def get_map(in_string):
             # (I will come back to this later)
             break
 
+        ch_vector = get_vector(ch)
         # Add new door to dict ret_val
+        curr_locn += ch_vector
+        ret_val[tuple(curr_locn)] = get_door(ch_vector)
+        
         # Add new room to dict ret_val
-        # Test both above to see if they're already there, and then test for any contradiction
+        curr_locn += ch_vector
+        ret_val[tuple(curr_locn)] = '.'
 
+        # Test both above to see if they're already there, and then test for any contradiction
 
     return ret_val
 
 
 def solve_problem(input_filename):
     in_string = get_input_line(input_filename)
-    get_map(in_string)
+    the_map = get_map(in_string)
     print(f'Without start and end characters: {in_string}')
     print()
 

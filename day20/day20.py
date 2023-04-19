cd0cde80c1cce03b714f97c083986799aab40348
Lewis Staples <lewis.staples@gmail.com>
2023-04-19 12:41:35 -0400

    Day 20  Added walls to map (in the bounding rectangle of areas occupied, plus in any unoccupied squares inside the bounding rectangle)

--- day20/day20.py
+++ day20/day20.py
@@ -12,7 +12,6 @@ def get_input_line(input_filename):
     with open(input_filename) as f:
         in_string = f.readline().rstrip()
     print(f'The original input is: {in_string}')
-    # return in_string[1:-1]
     return in_string
 
 
@@ -25,7 +24,6 @@ def get_vector(ch):
         case '$': return np.array([0,0])
         case ')': return np.array([0,0])
         case _:
-            # return np.array([0,0])
             raise ValueError(f'Character {ch} is not a compass direction!')
 
 def get_door(vector):
@@ -34,33 +32,51 @@ def get_door(vector):
     elif abs(vector[1]) == 1:
         return '-'
 
+def add_walls(the_map):
+
+    # Discover the perimeter:
+    min_x = float('inf')
+    max_x = float('-inf')
+    min_y = float('inf')
+    max_y = float('-inf')
+    for x,y in the_map.keys():
+        min_x = min(min_x, x)
+        max_x = max(max_x, x)
+        min_y = min(min_y, y)
+        max_y = max(max_y, y)
+
+    # Add outside walls (all one unit outside of the perimeter)
+    for x in range(min_x - 1, max_x + 2):
+        the_map[(x, min_y - 1)] = '#'
+        the_map[(x, max_y + 1)] = '#'
+    for y in range(min_y - 1, max_y + 2):
+        the_map[(min_x - 1, y)] = '#'
+        the_map[(max_x + 1, y)] = '#'
+
+    # Add walls for any spaces that haven't yet been visited
+    for y in range(max_y, min_y - 1, -1):
+        for x in range(min_x, max_x + 1):
+            if (x,y) not in the_map:
+                the_map[(x, y)] = '#'
+
 def get_map(in_string, i_init, curr_locn_in):
-    ret_val = dict()
+    the_map = dict()
     if curr_locn_in is None:
         curr_locn = np.array([0,0])
-        ret_val[tuple(curr_locn)] = 'X'
+        the_map[tuple(curr_locn)] = 'X'
     else:
         curr_locn = copy.deepcopy(curr_locn_in)
         
-        dummy = 123
-
-    # for i in range(i_init,len(in_string) + 1):
     i = i_init - 1
     while i < len(in_string) - 1:
         i += 1
-        if in_string[i] == '(': # in ['(','|']:
-            ret_val_new, i = get_map(in_string, i + 1, curr_locn)
-            ret_val |= ret_val_new
+        if in_string[i] == '(':
+            the_map_new, i = get_map(in_string, i + 1, curr_locn)
+            the_map |= the_map_new
         elif in_string[i] == '|':
             curr_locn[0] = curr_locn_in[0]
             curr_locn[1] = curr_locn_in[1]
             continue
-            # try:
-            #     if in_string[i] == '|':
-            #         i -= 1
-            #     break
-            # except IndexError:
-            #     break
         elif in_string[i] in [')','$']:
             break
 
@@ -68,17 +84,15 @@ def get_map(in_string, i_init, curr_locn_in):
             ch_vector = get_vector(in_string[i])
         except IndexError:
             break
-        # Add new door to dict ret_val
+        # Add new door to dict the_map
         curr_locn += ch_vector
-        ret_val[tuple(curr_locn)] = get_door(ch_vector)
+        the_map[tuple(curr_locn)] = get_door(ch_vector)
         
-        # Add new room to dict ret_val
+        # Add new room to dict the_map
         curr_locn += ch_vector
-        ret_val[tuple(curr_locn)] = '.'
+        the_map[tuple(curr_locn)] = '.'
 
-        # Test both above to see if they're already there, and then test for any contradiction
-
-    return ret_val, i + 1
+    return the_map, i + 1
 
 
 def display(the_map):
@@ -107,6 +121,7 @@ def display(the_map):
 def solve_problem(input_filename):
     in_string = get_input_line(input_filename)
     the_map, _ = get_map(in_string, 1, None)
+    add_walls(the_map)
     display(the_map)
     print(f'Without start and end characters: {in_string}')
     print()

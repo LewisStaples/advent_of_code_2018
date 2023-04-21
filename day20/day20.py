42e78d8404f9b67cf193bd77af84c3025dcf8b7b
Lewis Staples <lewis.staples@gmail.com>
2023-04-21 12:49:34 -0400

    Day 20  Getting answers to both part1 and part2

--- day20/day20.py
+++ day20/day20.py
@@ -6,6 +6,8 @@
 
 import numpy as np
 import copy
+import heapq as heap
+from functools import reduce
 
 def get_input_line(input_filename):
     print(f'\nUsing input file: {input_filename}\n')
@@ -31,7 +33,6 @@ def get_door(vector):
         return '-'
 
 def add_walls(the_map):
-
     # Discover the perimeter:
     min_x = float('inf')
     max_x = float('-inf')
@@ -56,6 +57,7 @@ def add_walls(the_map):
         for x in range(min_x, max_x + 1):
             if (x,y) not in the_map:
                 the_map[(x, y)] = '#'
+
 
 def get_map(in_string, i_init, curr_locn_in):
     the_map = dict()
@@ -84,7 +86,6 @@ def get_map(in_string, i_init, curr_locn_in):
         except IndexError:
             break
         except ValueError:
-            # print(f'ValueError occured at character # {i}')
             raise ValueError(f'ValueError occured at character # {i} ... \n\nprior_str: {in_string[:i]}\ntriggering character: {in_string[i]}\npost_str: {in_string[i+1:]}\noriginal_str:{in_string}\n')
         # Add new door to dict the_map
         curr_locn += ch_vector
@@ -98,6 +99,10 @@ def get_map(in_string, i_init, curr_locn_in):
 
 
 def display(the_map):
+    if len(the_map) > 400:
+        # It's too large to display
+        return
+    
     print('-------------------')
     min_x = float('inf')
     max_x = float('-inf')
@@ -120,11 +125,64 @@ def display(the_map):
         print()
     print('-------------------')
 
+
+def get_room_distances(the_map):
+    UNIT_VECTORS = (
+        np.array([1,0]),
+        np.array([-1,0]),
+        np.array([0,1]),
+        np.array([0,-1]),
+        )
+    DOOR_CHARACTERS = (
+        '|',
+        '|',
+        '-',
+        '-',
+    )
+    room_distances = dict()
+    latest_rooms = list()
+    first_room = (0,0)
+    heap.heappush(latest_rooms, (0, first_room))
+    room_distances[first_room] = 0
+
+    while len(latest_rooms) > 0:
+        door_count, latest_room_coords = heap.heappop(latest_rooms)
+        latest_room_coords = np.array(latest_room_coords)
+
+        for unit_vector, door_character in zip(UNIT_VECTORS, DOOR_CHARACTERS):
+            new_door_coords = latest_room_coords + unit_vector
+            if door_character == the_map[tuple(new_door_coords)]:
+                new_room_coords = tuple(new_door_coords + unit_vector)
+                if new_room_coords not in room_distances:
+                    room_distances[new_room_coords] = room_distances[tuple(latest_room_coords)] + 1
+                    # latest_rooms.add(new_room_coords)
+                    heap.heappush(latest_rooms, (door_count + 1, new_room_coords))
+
+    return room_distances
+
+
+def count_rooms_pathlen_thousandPlus(room_distances):
+    return len(list(filter(lambda x: x >= 1000, room_distances.values())))
+
+
+def print_results(room_distances):
+    print(f'The count of doors for shortest path to reach the furthest room')
+    print(f'(the answer to part 1) is: {max(room_distances.values())}\n')
+
+    print(f'The count of rooms whose shortest path has length 1000 or greater')
+    print(f'(the answer to part 2) is:', end = ' ') 
+    print(f'{count_rooms_pathlen_thousandPlus(room_distances)}\n')
+
 def solve_problem(input_filename):
     in_string = get_input_line(input_filename)
     the_map, _ = get_map(in_string, 1, None)
     add_walls(the_map)
     display(the_map)
     print()
+
+    room_distances = get_room_distances(the_map)
+    # print(f'Count of doors for shortest path to reach the furthest room (the answer to part 1) is: {max(room_distances.values())}\n')
+
+    print_results(room_distances)
 
 solve_problem('input_sample5.txt')

31fbb89793ba9160333db6e7643d0c65349a1c95
Lewis Staples <lewis.staples@gmail.com>
2023-04-23 23:31:44 -0400

    Day 22  Calculating geologic index, erosion level.  Creating a display that matches that given in the problem statement.

--- day22/day22.py
+++ day22/day22.py
@@ -24,10 +24,70 @@ def get_input(input_filename):
     print()
     return depth, target
 
+
+def get_erosion_level(geologic_index, depth):
+    return (geologic_index + depth) % 20183
+
+
+def get_regional_geologic_indices(depth, target):
+    regional_geologic_indices = list()
+    MAX_HEIGHT = max(16, target[1])
+    MAX_LENGTH = max(16, target[0])
+    
+    # Top row (height == 0)
+    regional_geologic_indices.append(list())
+    for length in range(MAX_LENGTH):
+        regional_geologic_indices[-1].append(16807 * length)
+
+    # Remaining rows
+    for height in range(1, MAX_HEIGHT):
+        regional_geologic_indices.append(list())
+        regional_geologic_indices[-1].append(48271 * height)
+        for length in range(1, MAX_LENGTH):
+            regional_geologic_indices[-1].append(
+                get_erosion_level(regional_geologic_indices[-1][length - 1], depth)
+                * get_erosion_level(regional_geologic_indices[-2][length], depth)
+            )
+            if (length, height) == target:
+                # Set target's geologic index to zero
+                regional_geologic_indices[-1][-1] = 0
+
+    return regional_geologic_indices
+
+
+def display(regional_geologic_indices, target, depth):
+    if len(regional_geologic_indices) > 20:
+        # Too much data to print on screen !
+        return
+    for row_num, row in enumerate(regional_geologic_indices):
+        for col_num, field in enumerate(row):
+            # Print mouth character
+            if (col_num, row_num) == (0,0):
+                print('M', end = '')
+                continue
+            # Print target character
+            if (col_num, row_num) == target:
+                print('T', end = '')
+                continue
+            
+            # Lookup geologic index, calculate erosion level, calculate mod 3 and then match 0,1,2
+            match get_erosion_level(regional_geologic_indices[row_num][col_num], depth) % 3:
+                # Rocky
+                case 0:
+                    print('.', end = '')
+                # Wet
+                case 1:
+                    print('=', end = '')
+                # Narrow
+                case 2:
+                    print('|', end = '')
+        print()
+    print()
+
 def solve_problem(input_filename):
     depth, target = get_input(input_filename)
-    print(f'{depth = }')
-    print(f'{target = }')
-    print()
+    regional_geologic_indices = get_regional_geologic_indices(depth, target)
+    display(regional_geologic_indices, target, depth)
+
 
 solve_problem('input_sample0.txt')

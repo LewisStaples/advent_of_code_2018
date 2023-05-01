f71c57bc36824534077b07c727ac0bdd2f2bca8d
Lewis Staples <lewis.staples@gmail.com>
2023-04-30 23:18:50 -0400

    Day 23  Converting input to data structures .... showing largest r value

--- day23/day23.py
+++ day23/day23.py
@@ -4,8 +4,11 @@
 # https://adventofcode.com/2018/day/23
 
 
-def get_input_line(input_filename):
-    # Reading input from the input file
+def get_input_DSs(input_filename):
+    pos_list = list()
+    r_list = list()
+
+    # Reading input
     print(f'\nUsing input file: {input_filename}\n')
     with open(input_filename) as f:
         # Pull in each line from the input file
@@ -13,13 +16,28 @@ def get_input_line(input_filename):
             in_string = in_string.rstrip()
 
             # Input line into data structures
-            print(in_string)
+            left_str, r_str = in_string.split(', r=')
+            pos_str = left_str[5:-1].split(',')
+            print(f'{in_string}   pos: {pos_str}   r: {r_str}')
+            r_list.append(int(r_str))
+
+            pos = tuple([
+                int(x)
+                for x in pos_str
+            ])
+            pos_list.append(pos)
+
+    r_max = max(r_list)
+    index__r_max = r_list.index(r_max)
+    print(f'Maximum r: {r_max}, Associated POS: {pos_list[index__r_max]}')
     print()
     
     # ADD ... return data structures with lines of input
+    return r_list, pos_list
     
 def solve_problem(input_filename):
-    in_string = get_input_line(input_filename)
+    r_list, pos_list = get_input_DSs(input_filename)
+
     # Determine the largest value of r, and the associated pos value
     
     # Calculate Manhattan distances for each pos from the largest r's pos
@@ -28,7 +46,7 @@ def solve_problem(input_filename):
     
 
 
-solve_problem('input.txt')
+solve_problem('input_sample0.txt')
 
 # def test_sample_0():
 #     solve_problem('input_sample0.txt')

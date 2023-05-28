84219b4f87cb022a2153cdc3cfb0ab5b43785805
Lewis Staples <lewis.staples@gmail.com>
2023-05-27 23:40:11 -0400

    Day 25 Function get_three_cube discovers the 4-dimension cube containing all points three units in any dimension from the original point.  This will be replaced with the Manhattan Distance

--- day25/day25.py
+++ day25/day25.py
@@ -4,14 +4,37 @@
 # https://adventofcode.com/2018/day/25
 
 
+import numpy as np
+import itertools
+
+def get_three_cube(this_point):
+    three_cube = set()
+    # print(this_point)
+    for dude in itertools.product(range(-3, 4), repeat = 4):
+        # print(np.array(dude))
+        new_point = this_point + np.array(dude)
+        three_cube.add(tuple(new_point))
+        # print(new_point)
+        # pass
+    return three_cube
+
 def get_constellation_count(input_filename):
+    constellation_list = [set()]
     # Reading input from the input file
     print(f'\nUsing input file: {input_filename}\n')
     with open(input_filename) as f:
         # Pull in each line from the input file
         for in_string in f:
-            this_point = [int(x) for x in in_string.rstrip().split(',')]
+            this_point = np.array([int(x) for x in in_string.rstrip().split(',')])
             print(this_point)
+            three_cube = get_three_cube(this_point)
+            indices_constellation = list()
+
+            for index_constellation, constellation in enumerate(constellation_list):
+                if len(constellation.intersection(three_cube)) > 0:
+                    indices_constellation.append(index_constellation)
+                    break
+            print()
     print()
 
     return 42
